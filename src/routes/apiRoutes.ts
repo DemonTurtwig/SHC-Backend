e33e457f3abef489abe05e7b95cb096ee7b7d018
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-04-25 13:35:06 +0900

    Update apiRoutes.ts

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,28 +1,21 @@
 // src/routes/apiRoutes.ts
 import express from 'express';
-import { registerUser, kakaoLogin, getOptions, getPricing, createBooking, getCurrentUser, loginUser} from '../controllers/apiController';
+import { registerUser, kakaoLogin, getOptions, getPricing, createBooking, getCurrentUser, loginUser, deleteUser} from '../controllers/apiController';
 import { requireAuth } from '../middleware/authMiddleware';
 import { getBookingInitializeData } from '../controllers/bookingController';
 
 
 const router = express.Router();
 
-// --- Auth
+
 router.post('/register', registerUser);
 router.post('/kakao/login', kakaoLogin);
 router.post('/login', loginUser);
-
-// --- Options
 router.get('/options', getOptions);       
-
-// --- Pricing
 router.get('/pricing', getPricing);
-
-// --- Booking
 router.post('/booking', createBooking); 
-
 router.get('/users/me', requireAuth, getCurrentUser);
-
 router.get('/booking/initialize', getBookingInitializeData);
+router.delete('/users/me', requireAuth, deleteUser);
 
 export default router;
