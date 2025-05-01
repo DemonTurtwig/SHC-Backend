6b1848834c585c43353bf5ca4994b61f0f325016
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-01 10:49:07 +0900

    Update Counter.ts

--- src/models/Counter.ts
+++ src/models/Counter.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const counterSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  seq: { type: Number, default: 100000 },
+  seq:  { type: Number, default: 0 },
 });
 
 const Counter = mongoose.model('Counter', counterSchema);
