200af3479b7d601bb9297ec00fc8b5d104e26c7a
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-01 10:49:37 +0900

    Update generateUserId.ts

--- src/utils/generateUserId.ts
+++ src/utils/generateUserId.ts
@@ -1,10 +1,20 @@
 import Counter from '../models/Counter';
+import User from '../models/User';
 
 export const generateUserId = async (): Promise<number> => {
-  const counter = await Counter.findOneAndUpdate(
+  // ① Try normal increment first
+  let counter = await Counter.findOneAndUpdate(
     { name: 'userId' },
     { $inc: { seq: 1 } },
-    { new: true, upsert: true }
+    { new: true }
   );
+
+  if (!counter) {
+    const max = await User.findOne().sort({ userId: -1 }).select('userId').lean();
+    const start = (Number(max?.userId) || 0) + 1;
+
+    counter = await Counter.create({ name: 'userId', seq: start });
+  }
+
   return counter.seq;
 };
