eff7d25a1df4ac794f4fbe2602b52f03c2353227
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-01 21:50:03 +0900

    Update kakaoService.ts

--- src/services/kakaoService.ts
+++ src/services/kakaoService.ts
@@ -1,29 +1,6 @@
 // src/services/kakaoService.ts
 import axios from 'axios';
 
-const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY as string;
-const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI as string;
-
-// 1. Exchange authorization code for access token
-export const getKakaoAccessToken = async (authorizationCode: string) => {
-  const response = await axios.post(
-    'https://kauth.kakao.com/oauth/token',
-    new URLSearchParams({
-      grant_type: 'authorization_code',
-      client_id: KAKAO_REST_API_KEY,
-      redirect_uri: KAKAO_REDIRECT_URI,
-      code: authorizationCode,
-    }),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-      },
-    }
-  );
-  return response.data.access_token;
-};
-
-// 2. Get user info using Kakao access token
 export const getKakaoUserInfo = async (accessToken: string) => {
   const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
     headers: {
@@ -33,7 +10,7 @@ export const getKakaoUserInfo = async (accessToken: string) => {
   return response.data;
 };
 
-export const searchAddress = async (query: string) => {
+export const searchKakaoAddress = async (query: string) => {
   const apiKey = process.env.KAKAO_REST_API_KEY;
 
   if (!apiKey) {
