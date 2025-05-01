3c952bc981b908d2782ce02b120b697556f7f082
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-01 21:47:08 +0900

    Delete src/routes/kakaoRoutes.ts

--- src/routes/kakaoRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { searchKakaoAddress } from '../controllers/kakaoController';
-
-const router = express.Router();
-
-router.get('/address', searchKakaoAddress);
-
-export default router;
