43606c6562041f6bb545881ecdcf05684ae04f73
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-01 15:02:15 +0900

    Update authMiddleware.ts

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -16,6 +16,7 @@ declare global {
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
   const token = req.headers.authorization?.split(' ')[1];
+   console.log('💡 In auth middleware, header =', req.headers.authorization);
   if (!token) {
     res.status(401).json({ message: 'No token provided' });
     return;
