647a0663c44b298da8f48d534d5fa16c7a513711
Allan Lee <113882539+DemonTurtwig@users.noreply.github.com>
2025-05-08 15:35:11 +0900

    Update apiRoutes.ts

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,14 +1,11 @@
 // src/routes/apiRoutes.ts
 import express from 'express';
 import { registerUser, updateUser, createBooking, getOptions, getPricing, getCurrentUser, loginUser, deleteUser, getAllServiceTypes, getAllTimeSlots, getUserBookingHistory} from '../controllers/apiController';
-import { kakaoLogin, searchKakaoAddress } from '../controllers/kakaoController';
+import { kakaoLogin, searchKakaoAddress, searchExpandedRoad } from '../controllers/kakaoController';
 import { requireAuth } from '../middleware/authMiddleware';
 import { getBookingInitializeData } from '../controllers/bookingController';
 
-
-
 const router = express.Router();
-
 
 router.post('/register', registerUser);
 router.post('/kakao/login', kakaoLogin);
@@ -24,4 +21,6 @@ router.delete('/users/me', requireAuth, deleteUser);
 router.get('/servicetypes', getAllServiceTypes);
 router.get('/history', requireAuth, getUserBookingHistory);
 router.patch('/users/me', requireAuth, updateUser);
+router.get('kakao/expand-address', searchExpandedRoad);
+
 export default router;
